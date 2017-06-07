1601c04c28b8e6c47815c7f5ea85cb37197d170c
Patrick Luboobi <patricklu2010@gmail.com>
2017-06-07 14:27:03 +0300

    Write functionality for getting all sublists

---
+++ enc.py
@@ -0,0 +1,20 @@
+def answer(s):
+    """
+    Encoder/Decoder method
+    """
+    if not isinstance(s, str):
+        raise TypeError('Argument should be of type String')
+
+    # Dictionary to hold the Key
+    key = {'a': 'z', 'b': 'y', 'c': 'x', 'd': 'w', 'e': 'v', 'f': 'u', 'g': 't', 'h': 's',
+           'i': 'r', 'j': 'q', 'k': 'p', 'l': 'o', 'm': 'n', 'n': 'm', 'o': 'l', 'p': 'k',
+           'q': 'j', 'r': 'i', 's': 'h', 't': 'g', 'u': 'f', 'v': 'e', 'w': 'd', 'x': 'c',
+           'y': 'b', 'z': 'a'}
+
+    dec_string = []
+    for ch in s:
+        letter = key.setdefault(ch, ch)
+        dec_string.append(letter)
+    return "".join(dec_string)
+
+
